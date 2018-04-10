4e98b3dfe8d6aaf3be46a714bc236358f7907efa
Denis Tokarev <blok1993@yandex.ru>
2018-04-11 00:16:52 +0300

    Fix

--- request.js
+++ request.js
@@ -1,6 +1,7 @@
 /**
- * Библиотека имеет публичный метод get с 3-мя аргументами: url, onResolve, onReject.
- * Гарантируется последовательное выполнение запросов (.get(...).get(...)), благодаря использованию промисов.
+ * Библиотека Request предназачена для совершения последовательных запросов.
+ * Имеется метод get, который можно чейнить.
+ * Каждый последующий запрос имеет доступ к данным, полученным из предыдущего запроса.
  */
 class Request {
     /**
@@ -18,52 +19,52 @@ class Request {
     _ajaxRequestStub(item) {
         return new Promise((resolve, reject) => {
             // Эмулируем время ответа сервера от 30 до 250 ms.
-            const timeout = Math.floor((Math.random() * 250) + 30);
+            const pendingTime = Math.floor((Math.random() * 250) + 30);
 
             setTimeout(() => {
-                console.log(`Requested url: ${item}, Pending time: ${timeout}ms`);
+                // Используем шаблонные строки для вывода логов.
+                console.log(`Requested url: ${item}, Pending time: ${pendingTime}ms`);
 
-                // Для примера вызываем resolve.
-                resolve(timeout);
-            }, timeout);
+                // Для примера вызываем resolve, и передаем туда время выполнения текущего запроса.
+                resolve(pendingTime);
+            }, pendingTime);
         });
     }
 
+    /**
+     * Метод get принимает 3 аргумента: url, onResolve, onReject.
+     * Гарантируется последовательное выполнение запросов (.get(...).get(...)), благодаря использованию промисов.
+     */
     get(url, onResolve, onReject) {
         this.promise = this.promise.then((prevRequestResult) => {
-                // Передаем в onResolve данные, полученные из предыдущего запроса.
-                onResolve(prevRequestResult);
-                return this._ajaxRequestStub(url);
-            }, (error) => {
-                onReject(error)
-            })
-            .catch(() => {
-                console.log("При обращении к серверу произошла ошибка.");
+            // Передаем в onResolve данные, полученные из предыдущего запроса.
+            onResolve(prevRequestResult);
+
+            return this._ajaxRequestStub(url);
+        })
+            .catch((error) => {
+                onReject(error);
             });
 
         return this;
     }
-};
+}
 
+// Создаем экземпляр класса Request.
 const exampleRequest = new Request();
 
-/**
- * Строка ниже закомментирована для демонстрации примера работы библиотеки.
- * Но ее необходимо будет раскомментировать, в момент подключения exampleRequest в другом файле проекта.
- */
-
-//export default exampleRequest;
-
+// Строку ниже необходимо будет раскомментировать в момент подключения exampleRequest в другом файле проекта.
+// export default exampleRequest;
 
 /*
  * Методы onResolveMethod и onRejectMethod для примера.
  */
 const onResolveMethod = (data) => {
-    console.log(data ? data : null);
+    console.log(data ? `Previous request data: ${data}` : null);
 };
 
 const onRejectMethod = (data) => {
-    console.error(data ? data : null);
+    console.log(data ? data : 'При обращении к серверу произошла ошибка.');
 };
 
 /**
@@ -73,4 +74,3 @@ exampleRequest
     .get('https://example1.com/one.json', onResolveMethod, onRejectMethod)
     .get('https://example2.com/two.json', onResolveMethod, onRejectMethod)
     .get('https://example3.com/three.json', onResolveMethod, onRejectMethod);
-
